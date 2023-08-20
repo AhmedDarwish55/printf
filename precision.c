dca4283b06cfe8945b0248447774543c00681ec7
NohaAdel32 <136817046+NohaAdel32@users.noreply.github.com>
2023-08-20 03:50:53 +0300

    Update precision.c

--- precision.c
+++ precision.c
@@ -1,7 +1,5 @@
-#include "main.h"
-
 /**
- * get_precision - calculates the precision for printing
+ * get_precision - Calculates the precision for printing
  * by Ahmed and Noha
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
@@ -9,44 +7,36 @@
  *
  * Return: Precision.
  */
-
 int get_precision(const char *format, int *i, va_list list)
 {
+	int curr_i = *i + 1;
+	int precision = -1;
 
-int curr_i = *i + 1;
-int precision = -1;
+	if (format[curr_i] != '.')
+		return (precision);
 
-if (format[curr_i] != '.')
-return (precision);
+	precision = 0;
 
-precision = 0;
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-{
+	*i = curr_i - 1;
 
-if (is_digit(format[curr_i]))
-{
-
-precision *= 10;
-precision += format[curr_i] – '0';
-
-else if (format[curr_i] == '*')
-{
-
-curr_i++;
-precision = va_arg(list, int);
-break;
-
+	return (precision);
 }
-else
 
-break;
-}
-
-
-*i = curr_i - 1;
-
-return (precision);
-
-}
 
